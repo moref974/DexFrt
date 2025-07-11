74a7001e2b4b7c6cd4594f4a3794d98be8c97a23
moref974 <moref974@gmail.com>
2025-07-11 09:25:07 +0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import bluvex from "../image/Bluvex.png"
 import { useNavigate } from "react-router-dom";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Aside from "./aside";
 import { useAccount } from "wagmi";
 function NavBar(){
     const navigate = useNavigate()
@@ -22,7 +23,6 @@ function NavBar(){
       {/* Left Side */}
       <div className="flex flex-wrap items-center gap-4">
         <img src={bluvex} className="h-10 w-10 rounded-full" alt="BluVex Logo" />
-
         <div
           className="font-sans text-blue-400 -ml-5 text-lg font-bold cursor-pointer hover:text-pink-400 transition"
           title="BluVex"
@@ -30,25 +30,8 @@ function NavBar(){
         >
           BluVex
         </div>
+        <Aside/>
 
-        <select
-          onChange={handleChange}
-          defaultValue=""
-          className="bg-[#1f0034] appearance-none text-purple-300 mt-1 rounded px-2 py-1 text-sm outline-none  cursor-pointer"
-        >
-          <option value="" disabled hidden>
-            Positions
-          </option>
-          <option value="addLiquidity">New Position</option>
-          <option value="ViewLiquidity">View Position</option>
-        </select>
-
-        <div
-          className="text-purple-300 -ml-10 mt-1 text-sm cursor-pointer hover:text-pink-400 transition"
-          onClick={() => navigate("Pool")}
-        >
-          Pool
-        </div>
       </div>
 
       {/* Right Side: Wallet Button */}
